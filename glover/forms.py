dd726222b2562b972aa3195cb917c3718e0192fe
Fraser Dempster <2331229D@student.gla.ac.uk>
2020-03-06 09:14:34 +0000

    added UserRegistrationForm and changed register.html

--- glover/forms.py
+++ glover/forms.py
@@ -1,20 +1,32 @@
 from django import forms
-from glover.models import Profile
 from django.contrib.auth.models import User
 
+from glover.choices import GenderChoices
+from glover.models import Profile
 
-class UserForm(forms.ModelForm):
+class UserRegistrationForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
+	confirm_password = forms.CharField(widget=forms.PasswordInput())
+	first_name = forms.CharField(required=True)
+	email = forms.CharField(required=True)
+	dob = forms.DateField()
+	gender = forms.ChoiceField(choices = GenderChoices.get_choices(), widget=forms.Select(), required=True)
+
+	def clean_confirm_password(self):
+		password1 = self.cleaned_data['password']
+		password2 = self.cleaned_data['confirm_password']
+
+		if not password1 == password2:
+			raise forms.ValidationError("The passwords do not match.")
+		return password2
+
+	def clean_email(self):
+		email = self.cleaned_data['email'].strip()
+
+		if not email.endswith('@student.gla.ac.uk'):
+			raise forms.ValidationError("Email must belong to a University of Glasgow account. ( @student.gla.ac.uk )")
+		return email
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password',)
-
-
-# haven't included course/interests/societies bc for some reason 
-# they are not populated in the register form 
-class UserProfileForm(forms.ModelForm):
-	class Meta:
-		model = Profile
-		fields = ('username', 'dob', 'gender', 'bio', 'year_in', 'location', 
-			'library_floor', 'looking_for', )
+		fields = ('username', 'first_name', 'email', 'gender', 'dob', 'password', 'confirm_password')
