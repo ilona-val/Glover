98ba8ddcccc113579e1ddf0d29510c7472df2985
Tomas Mikus <2473026m@student.gla.ac.uk>
2020-03-23 13:50:11 +0000

    Add profiles to admin page

--- glover/admin.py
+++ glover/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 
-from glover.models import Like, Match
+from glover.models import Like, Match, Profile
 
 # Register your models here.
 class LikeAdmin(admin.ModelAdmin):
-	list_display = ('profile', 'profile_liked', 'is_liked')
+    list_display = ('profile', 'profile_liked', 'is_liked')
 
 
 class MatchAdmin(admin.ModelAdmin):
-	list_display = ('profile1', 'profile2', 'time_matched')
+    list_display = ('profile1', 'profile2', 'time_matched')
 
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'dob', 'gender',)
 
 
 admin.site.register(Like, LikeAdmin)
 admin.site.register(Match, MatchAdmin)
+admin.site.register(Profile, ProfileAdmin)
