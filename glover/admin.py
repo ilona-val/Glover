df591276c681ca840a0ae5a160ca44b1f5d10d7b
ilona-val <ilona.valentukeviciute@gmail.com>
2020-03-31 20:41:49 +0100

    updated admin page

--- glover/admin.py
+++ glover/admin.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
 
-from glover.models import Like, Match, Profile
+from glover.models import Like, Match, Profile, Course, Interest, Society
+
 
 # Register your models here.
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('profile', 'profile_liked', 'is_liked')
 
-
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('profile1', 'profile2', 'time_matched')
 
-
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'dob', 'gender',)
+    list_display = ('user', 'dob', 'gender', 'looking_for', 'course', 'year_in', 'library_floor', 'location',)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course',)
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('interest',)
+
+class SocietyAdmin(admin.ModelAdmin):
+    list_display = ('society',)
 
 
 admin.site.register(Like, LikeAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Interest, InterestAdmin)
+admin.site.register(Society, SocietyAdmin)
